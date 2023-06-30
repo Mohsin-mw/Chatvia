1b686a409f7552b4c916bde4afa71e2e9bf9c5d6
MMW <null.mmw@gmail.com>
2023-06-29 17:25:10 -0700

    Flowbite Setup Finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { Button } from "flowbite-react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <div>
+        <div className="text-blue-500">Hello</div>
+        <Button color="info">Good to Go!</Button>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -29,7 +32,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  content: ["node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}"],
+  plugins: [require("flowbite/plugin")],
+};
