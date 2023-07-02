771f0a028480d1d4f16594686bd1f55a6b1511f3
MMW <null.mmw@gmail.com>
2023-07-02 15:04:29 -0700

    TextMessage Click to send Message

--- src/components/SendMessage.tsx
+++ src/components/SendMessage.tsx
@@ -1,17 +1,26 @@
 import { PaperAirplaneIcon } from "@heroicons/react/24/outline";
+import { useState } from "react";
 
 const SendMessage = () => {
+  const [value, setValue] = useState("");
+  const HandleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log(value);
+    setValue("");
+  };
   return (
-    <div className="w-full flex flex-row px-5 py-3">
+    <form onSubmit={HandleSubmit} className="w-full flex flex-row px-5 py-3">
       <input
         type="text"
         placeholder="Type here"
         className="input input-bordered bg-neutral  w-full"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
       />
-      <button className="btn btn-square mx-2 bg-primary-shade-2">
+      <button type="submit" className="btn btn-square mx-2 bg-primary-shade-2">
         <PaperAirplaneIcon className="h-6 w-6 text-neutral-50" />
       </button>
-    </div>
+    </form>
   );
 };
 
