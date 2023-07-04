5d7707bf6fc6bdec1adea47120694cea0fb1a517
MMW <null.mmw@gmail.com>
2023-07-03 23:09:23 -0700

    Firebase added

---
+++ src/services/firebase.ts
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAPWkH0CaVLep3pTIq1ziV1GeNcIONlfCk",
+  authDomain: "chatvia-5a047.firebaseapp.com",
+  projectId: "chatvia-5a047",
+  storageBucket: "chatvia-5a047.appspot.com",
+  messagingSenderId: "315135155009",
+  appId: "1:315135155009:web:d9098bd6a32bcbd189dfc0",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
